a33b3cdf519dc29267aa130fa98e06fa1f6263f4
Drew Roberts <github@drewroberts.com>
2025-01-16 13:02:40 -0500

    Rewrite erc-420 contract

--- 420.sol
+++ 420.sol
@@ -1,95 +1,195 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.28;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v5.2/contracts/token/ERC1155/ERC1155.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v5.2/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v5.2/contracts/token/ERC20/utils/SafeERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v5.2/contracts/utils/ReentrancyGuard.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v5.2/contracts/utils/Strings.sol";
 
-contract ERC420 is ERC1155 {
-    uint256 public currentTokenId;
+contract ERC420 is ERC1155, Ownable, ReentrancyGuard {
+    using SafeERC20 for IERC20;
 
-    // Mapping to track the maximum supply of each token ID
-    mapping(uint256 => uint256) public maxSupply;
+    struct mintToken_data {
+        uint256 mintID;
+        address[] tokenAddress;
+    }
 
-    // Mapping to track the total minted supply of each token ID
-    mapping(uint256 => uint256) public mintedSupply;
+    bool public mintActive;
 
-    // Mapping to track vesting start times for each token ID
-    mapping(uint256 => uint256) public vestingStart;
+    mapping(address => uint256) public allocatedContractTokens;
+    
+    uint256 public currentMintID;
+    string public mintURI = "ipfs://bafkreifpc6oda3eqfq6plborptqkn5sfewx7xky44blziiwzy4nubx7vfe/";
 
-    // Mapping to track the last withdrawal time for each token ID
-    mapping(uint256 => uint256) public lastWithdrawalTime;
+    uint256 public mintPrice = .1 * (10 ** 18);
+    uint256 public maxMintSupply = 1000;
+    uint256 public currentMintSupply;
 
-    // Vesting and withdrawal constants
-    uint256 public constant VESTING_PERIOD = 90 days;
-    uint256 public constant WITHDRAWAL_INTERVAL = 30 days;
-    uint256 public constant WITHDRAWAL_PERCENTAGE = 10;
+    mapping(uint256 => mintToken_data) public mintTokens;
+    mapping(uint256 => mapping(address => uint256)) public mintTokenBalance;
+    mapping(uint256 => mapping(address => uint256)) public mintTokenVestDate;
+    mapping(uint256 => mapping(address => uint256)) public mintTokenLastWithdraw;
 
-    // constructor(string memory uri) ERC1155(uri) {}
-    constructor(string memory uri) ERC1155("ipfs://bafkreifpc6oda3eqfq6plborptqkn5sfewx7xky44blziiwzy4nubx7vfe") {}
+    uint256 public constant vestingPeriod = 90 days;
+    uint256 public constant vestingInterval = 30 days;
+    uint256 public constant vestingWithdrawPct = 10;
 
+    event adminAction(string Action, address Address, uint256 Amount);
+    event userAction(string actionTaken_, address actionAddress, uint256 actionValue_, bool actionResult_);
 
-    /**
-     * @dev Create a new token type
-     * @param _initialSupply The initial supply of tokens to mint
-     * @param _maxSupply The maximum supply for the token type
-     * @param _data Data to be passed if recipient is a contract
+    constructor() Ownable(msg.sender) ERC1155(mintURI) {}
+
+    /*
+     * Admin functions
      */
-    function createToken(uint256 _initialSupply, uint256 _maxSupply, bytes memory _data) external {
-        require(_maxSupply > 0, "Max supply must be greater than zero");
-        require(_initialSupply <= _maxSupply, "Initial supply cannot exceed max supply");
 
-        uint256 tokenId = ++currentTokenId;
-        maxSupply[tokenId] = _maxSupply;
-        mintedSupply[tokenId] = _initialSupply;
-        vestingStart[tokenId] = block.timestamp;
+    // Clear WETH from contract
+    function clearWETH() external onlyOwner nonReentrant {
+        uint256 fullBalance = address(this).balance;
+        (bool sendSuccess, ) = payable(owner()).call{value: fullBalance}("");
 
-        if (_initialSupply > 0) {
-            _mint(msg.sender, tokenId, _initialSupply, _data);
+        if (sendSuccess) {
+            emit adminAction("clearWETH", address(0), fullBalance);
+        } else {
+            emit adminAction("clearWETH", address(0), 0);
         }
     }
 
-    /**
-     * @dev Mint additional tokens of an existing type
-     * @param _tokenId The token ID to mint
-     * @param _amount The amount of tokens to mint
-     * @param _data Data to be passed if recipient is a contract
-     */
-    function mint(uint256 _tokenId, uint256 _amount, bytes memory _data) external {
-        require(maxSupply[_tokenId] > 0, "Token ID does not exist");
-        require(mintedSupply[_tokenId] + _amount <= maxSupply[_tokenId], "Minting would exceed max supply");
+    // Remove full balance of given token
+    function clearE20Token(address token_) external onlyOwner nonReentrant {
+        uint256 availableBalance = IERC20(token_).balanceOf(address(this)) - allocatedContractTokens[token_];
+        require(availableBalance > 0, "No available tokens");
 
-        mintedSupply[_tokenId] += _amount;
-        _mint(msg.sender, _tokenId, _amount, _data);
+        IERC20(token_).safeTransfer(owner(), availableBalance);
+
+        emit adminAction("clearE20Token", token_, availableBalance);
     }
 
-    /**
-     * @dev Burn tokens of a specific type
-     * @param _tokenId The token ID to burn
-     * @param _amount The amount of tokens to burn
-     */
-    function burn(uint256 _tokenId, uint256 _amount) external {
-        require(balanceOf(msg.sender, _tokenId) >= _amount, "Insufficient balance to burn");
-        _burn(msg.sender, _tokenId, _amount);
-        mintedSupply[_tokenId] -= _amount;
+    // Enable or disable new minting
+    function setMintActive(bool mintActive_) external onlyOwner {
+        mintActive = mintActive_;
+
+        emit adminAction("setMintActive", msg.sender, mintActive_ ? 0 : 1 );
     }
 
-    /**
-     * @dev Withdraw 10% of all ERC-20 tokens held by the contract for a specific token ID.
-     * @param _tokenAddress The address of the ERC-20 token to withdraw.
-     * @param _tokenId The ID of the ERC-1155 token to use for withdrawal logic.
-     */
-    function withdrawERC20(address _tokenAddress, uint256 _tokenId) external {
-        require(balanceOf(msg.sender, _tokenId) > 0, "Caller does not own this token ID");
-        require(vestingStart[_tokenId] + VESTING_PERIOD <= block.timestamp, "Vesting period not yet completed");
-        require(lastWithdrawalTime[_tokenId] + WITHDRAWAL_INTERVAL <= block.timestamp, "Withdrawal interval not yet elapsed");
+    // Deposit tokens to a specific mint
+    // TOKEN APPROVAL MUST HAPPEN EXTERNALLY FIRST
+    function depositTokens(uint256 mintID_, address tokenAddress_, uint256 tokenAmount_) external onlyOwner {
+        mintTokens[mintID_].mintID = mintID_;
 
-        IERC20 token = IERC20(_tokenAddress);
-        uint256 contractBalance = token.balanceOf(address(this));
-        uint256 withdrawalAmount = (contractBalance * WITHDRAWAL_PERCENTAGE) / 100;
+        bool newToken = true;
+        for (uint256 ct; ct < mintTokens[mintID_].tokenAddress.length; ct++) {
+            if (mintTokens[mintID_].tokenAddress[ct] == tokenAddress_) {
+                newToken = false;
+                break;
+            }
+        }
 
-        require(withdrawalAmount > 0, "No tokens available for withdrawal");
+        if (newToken) {
+            mintTokens[mintID_].tokenAddress.push(tokenAddress_);
+            mintTokenVestDate[mintID_][tokenAddress_] = block.timestamp + vestingPeriod;
+        }
 
-        lastWithdrawalTime[_tokenId] = block.timestamp;
-        token.transfer(msg.sender, withdrawalAmount);
+        IERC20(tokenAddress_).safeTransferFrom(msg.sender, address(this), tokenAmount_);
+        mintTokenBalance[mintID_][tokenAddress_] += tokenAmount_;
+        allocatedContractTokens[tokenAddress_] += tokenAmount_;
+
+        emit adminAction("depositTokens", tokenAddress_, tokenAmount_);
     }
+
+    // Allow contract owner to mint to a specific address
+    function newMintToUser(address mintRecipient_) external onlyOwner {
+        require(mintRecipient_ != address(0), "Cannot mint to 0 address");
+
+        uint256 adminMintID = doMint(mintRecipient_);
+
+        emit adminAction("newMintToUser", mintRecipient_, adminMintID);
+    }
+
+    /*
+     * User Functions
+     */
+    
+    // Mint NFT
+    function newMint() external payable nonReentrant {
+        require(msg.value >= mintPrice, "Not enough ETH");
+
+        uint256 userMintID = doMint(msg.sender);
+
+        emit userAction("newMint", msg.sender, userMintID, true);
+    }
+
+    // Burn NFT
+    function burnMint(uint256 mintID_) external {
+        require(balanceOf(msg.sender, mintID_) > 0, "Nothing to burn");
+
+        for (uint256 ct; ct < mintTokens[mintID_].tokenAddress.length; ct++) {
+            address curToken = mintTokens[mintID_].tokenAddress[ct];
+            allocatedContractTokens[curToken] -= mintTokenBalance[mintID_][curToken];
+            mintTokenBalance[mintID_][curToken] = 0;
+        }
+
+        delete mintTokens[mintID_];
+
+        _burn(msg.sender, mintID_, 1);
+
+        currentMintSupply--;
+
+        emit userAction("burnMint", msg.sender, mintID_, true);
+    }
+
+    // Withdraw tokens from a mint
+    function withdrawTokensFromMint(uint256 mintID_) external {
+        require(balanceOf(msg.sender, mintID_) > 0, "Sender does not own this mint");
+
+        for (uint256 ct; ct < mintTokens[mintID_].tokenAddress.length; ct++) {
+            address curToken = mintTokens[mintID_].tokenAddress[ct];
+            if (
+                block.timestamp >= mintTokenVestDate[mintID_][curToken] &&
+                block.timestamp >= mintTokenLastWithdraw[mintID_][curToken] + vestingInterval
+            ) {
+                uint256 withdrawAmount = (mintTokenBalance[mintID_][curToken] * vestingWithdrawPct) / 100;
+                IERC20(curToken).safeTransfer(msg.sender, withdrawAmount);
+                mintTokenBalance[mintID_][curToken] -= withdrawAmount;
+                allocatedContractTokens[curToken] -= withdrawAmount;
+                mintTokenLastWithdraw[mintID_][curToken] = block.timestamp;
+
+                emit userAction("withdrawTokensFromMint1", msg.sender, mintID_, true);
+                emit userAction("withdrawTokensFromMint2", curToken, withdrawAmount, true);
+            } else {
+                emit userAction("withdrawTokensFromMint1", msg.sender, mintID_, false);
+                emit userAction("withdrawTokensFromMint2", curToken, 0, false);
+            }
+        }
+    }
+
+    /*
+     * Support Functions
+     */
+    
+    // Perform mint actions
+    function doMint(address mintRecipient_) internal returns (uint256) {
+        require(currentMintSupply < maxMintSupply, "Cannot mint any more NFTs");
+        require(mintActive || msg.sender == owner(), "Minting is currently unavailable");
+
+        uint256 newMintID = ++currentMintID;
+        _mint(mintRecipient_, newMintID, 1, "");
+        
+        currentMintSupply++;
+
+        return newMintID;
+    }
+    
+    // Return URI as string
+    function uri(uint256 mintID_) override public view returns (string memory) {
+        return string(abi.encodePacked(mintURI, Strings.toString(mintID_),".json"));
+    }
+
+    // List all tokens associated with a mint
+    function showMintTokens(uint256 mintID_) external view returns (address[] memory) {
+        return mintTokens[mintID_].tokenAddress;
+    }
+
+    // KDR-20250115
 }
